9117d903997d3c1fb1b7870e400e57204b0fd3f5
Pixote <guiihpr@gmail.com>
2020-11-23 16:16:18 -0300

    removed unused log error

--- src/app.js
+++ src/app.js
@@ -62,9 +62,4 @@ const init = async () => {
   console.log('Server running on %s', server.info.uri)
 }
 
-process.on('unhandledRejection', (err) => {
-  console.log(err)
-  process.exit(1)
-})
-
 module.exports = init
